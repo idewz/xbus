1d94f78738340691d07e2ee16517894516cd6f99
Siwadon Saosoong <siwadon.s@gmail.com>
2017-04-15 15:03:55 -0700

    Implement agency list fetching
    - be able to fetch agency list and get the agency by tag id
    - use nock for HTTP mocking https://github.com/node-nock/noc
    - remove flow type for now due to slower WebStorm performance

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
-// @flow
+"use strict"
 
-module.exports = {
-    agencies: function() {
-        return [];
-    },
-}
+const request = require('superagent');
+
+module.exports = class XBus {
+    base_url;
+
+    constructor() {
+        this.base_url = 'http://webservices.nextbus.com/service/publicJSONFeed';
+    }
+
+    async getAgencies() {
+        const qs = { command: 'agencyList' };
+
+        try {
+            const res = await request.get(this.base_url).query(qs);
+            return res.body.agency;
+        }
+        catch (err) {
+            console.error(err);
+            return [];
+        }
+    }
+
+    async getAgency(tag) {
+        const agencies = await this.getAgencies();
+
+        return agencies.find(a => a.tag === tag);
+    }
+};
+
--- test/index.js
+++ test/index.js
@@ -1,9 +1,42 @@
-var should = require('chai').should(),
-    xbus = require('../src/index'),
-    agencies = xbus.agencies();
+"use strict";
 
-describe('agencies', function() {
-    it('get agencies list', function() {
-        agencies.length.should.equal(0);
+var should = require('chai').should(),
+    nock = require('nock'),
+    XBus = require('../lib/index');
+
+var xbus = new XBus();
+
+describe('agencies', function () {
+    beforeEach(function () {
+        const agencyList = nock('http://webservices.nextbus.com')
+            .get('/service/publicJSONFeed')
+            .query({command: 'agencyList'})
+            .reply(200, {
+                    agency: [
+                        {
+                            "title": 'San Francisco Muni',
+                            "regionTitle": "California-Northern",
+                            "shortTitle": "SF Muni",
+                            "tag": 'sf-muni'
+                        },
+                        {
+                            "title": "AC Transit",
+                            "regionTitle": "California-Northern",
+                            "tag": "actransit"
+                        }
+                    ]
+                },
+                {'Content-Type': 'application/json'}
+            );
+    });
+
+    it('get agencies list', async function () {
+        const agencies = await xbus.getAgencies();
+        agencies.length.should.equal(2);
+    });
+
+    it('get agency by tag id', async function () {
+        const agency = await xbus.getAgency('sf-muni');
+        agency.title.should.equal('San Francisco Muni');
     });
 });
